7a0066612450cbfe5d107000e422b8baf686109d
haidark <haidark@gmail.com>
2015-06-03 20:30:34 -0400

    added datepicker

--- android/app/src/main/java/com/masjidumar/masjid/MainActivity.java
+++ android/app/src/main/java/com/masjidumar/masjid/MainActivity.java
@@ -1,17 +1,25 @@
 package com.masjidumar.masjid;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.provider.DocumentsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -41,42 +49,21 @@ import javax.xml.transform.stream.StreamResult;
 public class MainActivity extends ActionBarActivity {
     public final static String EXTRA_MESSAGE="com.masjidumar.masjid.MESSAGE";
     ProgressDialog pDialog;
-
+    static int month;
+    static int day;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
         //try to load timings from the previously downloaded file
         Calendar cal = new GregorianCalendar();
-        int month = cal.get(Calendar.MONTH) + 1;
-        Document doc = null;
-        try {
-            FileInputStream xmlFile = new FileInputStream(new File(getCacheDir(), "j"+month+".xml"));
-            //Build a DOM
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
+        month = cal.get(Calendar.MONTH) + 1;
+        day = cal.get(Calendar.DAY_OF_MONTH);
 
-            // Open the XML file
-            doc = db.parse(new InputSource(xmlFile));
-            xmlFile.close();
-        } catch(FileNotFoundException f){
-            //if the file is not found, download it.
-            Log.d("IO", "File not found");
-            new DownloadTimingsXML().execute();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        Button datepicker = (Button) findViewById(R.id.pickDate);
+        datepicker.setText(month + "/" + day);
 
-        setContentView(R.layout.activity_main);
-        String txt;
-        if(doc != null) {
-            txt = extractTimings(doc);
-        }else{
-            txt = "ERROR!";
-        }
-
-        TextView tView = (TextView) findViewById(R.id.xmlView);
-        tView.setText(txt);
+        updateTimings();
     }
 
     @Override
@@ -106,22 +93,37 @@ public class MainActivity extends ActionBarActivity {
         new DownloadTimingsXML().execute();
     }
 
+    public void updateTimings(){
+        Document doc = null;
+        try {
+            FileInputStream xmlFile = new FileInputStream(new File(getCacheDir(), "j"+month+".xml"));
+            //Build a DOM
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            // Open the XML file
+            doc = db.parse(new InputSource(xmlFile));
+            xmlFile.close();
+        } catch(FileNotFoundException f){
+            //if the file is not found, download it.
+            Log.d("IO", "File not found");
+            new DownloadTimingsXML().execute();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        if(doc != null) {
+            extractTimings(doc);
+        }
+    }
+
     private class DownloadTimingsXML extends AsyncTask<Void, Void, Document> {
         String xmlJURL = getString(R.string.jamaat_URL);
         String xmlPURL = getString(R.string.prayer_URL);
 
         @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-        }
-
-        @Override
         protected Document doInBackground(Void... Params){
-
             try{
-                //get this month
-                Calendar cal = new GregorianCalendar();
-                int month = cal.get(Calendar.MONTH)+1; //add 1 since it starts at 0
 
                 //Form the URL for this month
                 URL url = new URL(xmlJURL+Integer.toString(month)+".xml");
@@ -147,60 +149,101 @@ public class MainActivity extends ActionBarActivity {
                 e.printStackTrace();
                 return null;
             }
-
         }
 
         @Override
         protected void onPostExecute(Document doc){
-            String txt;
             if(doc != null) {
-                txt = extractTimings(doc);
-            }else{
-                txt = "ERROR!";
+                extractTimings(doc);
             }
-
-            TextView tView = (TextView) findViewById(R.id.xmlView);
-            tView.setText(txt);
         }
-
-
     }
 
-    public String extractTimings(Document doc) {
+    public void extractTimings(Document doc) {
         doc.getDocumentElement().normalize(); //???
         // get all the date elements
         NodeList nodes = doc.getElementsByTagName("date");
         //extract timings.
-        String txt = "";
-        Calendar cal = new GregorianCalendar();
-        int j = cal.get(Calendar.DAY_OF_MONTH)-1;
+        String txt;
+        TextView view;
 
+        int j = day-1;
         Element dateElem = (Element) nodes.item(j);
-        txt = txt + "Last Updated: " + cal.get(Calendar.HOUR) + ":" + cal.get(Calendar.MINUTE) + "\n";
 
+        //Fajr
         NodeList fajr = dateElem.getElementsByTagName("fajr");
         String fajrJTime = fajr.item(0).getChildNodes().item(0).getNodeValue();
-        txt = txt + getString(R.string.fajr) + ": " + fajrJTime + "\n";
+        txt = ": " + fajrJTime + "\n";
+        view = (TextView) findViewById(R.id.fajrTime);
+        view.setText(txt);
 
+        //sunrise
         NodeList sunrise = dateElem.getElementsByTagName("sunrise");
         String sunriseTime = sunrise.item(0).getChildNodes().item(0).getNodeValue();
-        txt = txt + getString(R.string.sunrise) + ": " + sunriseTime + "\n";
+        txt = ": " + sunriseTime + "\n";
+        view = (TextView) findViewById(R.id.sunriseTime);
+        view.setText(txt);
 
+        //dhuhr
         NodeList dhuhr = dateElem.getElementsByTagName("dhuhr");
         String dhuhrJTime = dhuhr.item(0).getChildNodes().item(0).getNodeValue();
-        txt = txt + getString(R.string.dhuhr) + ": " + dhuhrJTime  + "\n";
+        txt = ": " + dhuhrJTime  + "\n";
+        view = (TextView) findViewById(R.id.dhuhrTime);
+        view.setText(txt);
 
+        //asr
         NodeList asr = dateElem.getElementsByTagName("asr");
         String asrJTime = asr.item(0).getChildNodes().item(0).getNodeValue();
-        txt = txt + getString(R.string.asr) + ": " + asrJTime + "\n";
+        txt = ": " + asrJTime + "\n";
+        view = (TextView) findViewById(R.id.asrTime);
+        view.setText(txt);
 
+        //maghrib
         NodeList maghrib = dateElem.getElementsByTagName("maghrib");
         String maghribJTime = maghrib.item(0).getChildNodes().item(0).getNodeValue();
-        txt = txt + getString(R.string.maghrib) + ": " + maghribJTime + "\n";
+        txt = ": " + maghribJTime + "\n";
+        view = (TextView) findViewById(R.id.maghribTime);
+        view.setText(txt);
 
+        //isha
         NodeList isha = dateElem.getElementsByTagName("isha");
         String ishaJTime = isha.item(0).getChildNodes().item(0).getNodeValue();
-        txt = txt + getString(R.string.isha) + ": " + ishaJTime + "\n";
-        return txt;
+        txt = ": " + ishaJTime + "\n";
+        view = (TextView) findViewById(R.id.ishaTime);
+        view.setText(txt);
+    }
+
+    /* Date Picker Fragment */
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int m, int d) {
+            month = m + 1;
+            day = d;
+
+        }
+    }
+
+    public void pickDate(View v){
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+        updateTimings();
+        Button datepicker = (Button) findViewById(R.id.pickDate);
+        datepicker.setText(month + "/" + day);
+
+        View main = findViewById(R.id.mainActivity);
+        main.invalidate();
     }
 }
