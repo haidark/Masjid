edeb09954e64626123b6e2a2608ce952c42a944f
unknown <tanvir9912@gmail.com>
2023-03-21 08:41:11 -0400

    Auto_hijri update

--- MasjidUmarWebsiteFirebase/public/chicagohilalwebscrape.js
+++ MasjidUmarWebsiteFirebase/public/chicagohilalwebscrape.js
@@ -1,46 +1,72 @@
+/*const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+async function scrapeWeb() {
+  axios.get('https://chicagohilal.org/')
+    .then(response => {
+      const $ = cheerio.load(response.data);
+      const bodyText = $('body').text().trim();
+
+      // Find the specific word "Ramadan" in the body text
+      if (bodyText.includes('144')) {
+        const ramadanDate = bodyText.split('14')[0] + '144'; // Get the text before and including the word "Ramadan"
+        const htmlData = `<html><body><h1>${ramadanDate}</h1></body></html>`;
+
+        // Write the new date to the output.html file
+        fs.writeFile('output.html', htmlData, (err) => {
+          if (err) throw err;
+          console.log('The Ramadan date has been written to output.html');
+        });
+      } else {
+        console.log('The word "Ramadan" has not been found yet.');
+      }
+    })
+    .catch(error => console.error(error));
+
+
+
+    var fulldate = "1444";
+    document.getElementById("hijridateval").innerText = fulldate;
+    document.getElementById("hijridateval").textContent = fulldate;
+    
+}
+
+setInterval(scrapeWeb, 1000); //time is in ms
+*/
 
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const { JSDOM } = require('jsdom');
 
-//const url = 'https://chicagohilal.org/';
-async function showscrapeWeb(){
-axios.get('https://chicagohilal.org/')
-  .then(response => {
+async function scrapeWeb() {
+  axios.get('https://chicagohilal.org/')
+    .then(response => {
+      const $ = cheerio.load(response.data);
+      const bodyText = $('body').text().trim();
 
+      // Find the specific word "1444" in the body text
+      if (bodyText.includes('1444')) {
+        const hijriDate = bodyText.split('1444')[0] + '1444'; // Get the text before and including the word "Ramadan"
+        const htmlData = `<html><body><h1>${hijriDate}</h1></body></html>`;
 
+        // Write the new date to the output.html file
+        fs.writeFile('output.html', htmlData, (err) => {
+          if (err) throw err;
+          console.log('The Ramadan date has been written to output.html');
+        });
+      } else {
+        console.log('The word "Ramadan" has not been found yet.');
+      }
+    })
+    .catch(error => console.error(error));
 
-
-    const $ = cheerio.load(response.data);
-    const hijriDate = $('body').text().trim();
-    const htmlData = `<html><body><h1>adaw${hijriDate}</h1></body></html>`;
-    fs.writeFile('output.html', htmlData, (err) => {
-      if (err) throw err;
-      console.log('The hijri date has been written to output.html');
-    //  setTimeout(showscrapeWeb, 1000);
-    });
-    
-
-    var fulldate = htmlData;// using to test to see if library does affect it and it does
-   // document.getElementById("MytitleDisplay").innerText = fulldate;
-   // document.getElementById("MytitleDisplay").textContent = fulldate;
-
-    
-
-
-    //setTimeout(showscrapeWeb, 1000);
-  
-
-     
-
-
-
-
-  })
-
-  .catch(error => console.error(error));
-
+ 
+  //setTimeout(scrapeWeb, 1000);
+ 
 }
+//setInterval(scrapeWeb, 1000); //time is in ms
 
-//showscrapeWeb();
-setInterval(showscrapeWeb, 1000); //time is in ms
+setInterval(scrapeWeb, 60000); //time is in ms
+scrapeWeb();
